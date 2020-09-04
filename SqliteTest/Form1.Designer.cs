7f2392dd4e23c9514462aedc2c58923c4aff897c
NIMO <laomms@gmail.com>
2020-09-04 11:25:28 +0700

    添加项目文件。

---
+++ SqliteTest/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace SqliteTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "创建数据库";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 99);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(534, 187);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(228, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 25);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "读取数据库";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(336, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 25);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "修改数据库";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(444, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 25);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "删除数据库";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(120, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(102, 25);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "插入数据库";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(61, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(236, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(128, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "EMAIL:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(419, 59);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(124, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(375, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "CODE:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 298);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SqliteTest/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqliteTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(Application.StartupPath + "\\sqlite3.dll") == false)
+            {
+                System.IO.File.WriteAllBytes(Application.StartupPath + "\\sqlite3.dll", Properties.Resources.sqlite3);
+            }
+
+            this.listView1.Items.Clear();
+            this.listView1.GridLines = true;
+            this.listView1.View = View.Details;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Columns.Add("ID", 30, HorizontalAlignment.Center);
+            this.listView1.Columns.Add("用户ID", 100, HorizontalAlignment.Left);
+            this.listView1.Columns.Add("用户名", 100, HorizontalAlignment.Left);
+            this.listView1.Columns.Add("密码", 100, HorizontalAlignment.Left);
+            this.listView1.Columns.Add("时间", listView1.Width-30-300, HorizontalAlignment.Center);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var tablevalue = new List<string[]>() {
+                new string[]{ "`Name` TEXT", "`EMAIL` TEXT", "`CODE` TEXT"},
+                new string[]{ "`用户ID` TEXT", "`用户名` TEXT", "`密码` TEXT", "`时间` TEXT" }
+            };
+
+            SqliHelper.CreateTable(new string[] { "table1", "表2" }, tablevalue);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (SqliHelper.CheckDataExsit("table1", "Name","aaa")==false)
+            {
+                SqliHelper.InsertData("table1", new string[] { "Name", "EMAIL", "CODE" }, new string[] { "aaa", "aaa@.com", "1234" });
+                SqliHelper.InsertData("table1", new string[] { "Name", "EMAIL", "CODE" }, new string[] { "bbb", "aaa@.com", "5678" });
+                SqliHelper.InsertData("table1", new string[] { "Name", "EMAIL", "CODE" }, new string[] { "ccc", "aaa@.com", "6666" });
+            }
+            if (SqliHelper.CheckDataExsit("table1", "用户ID", "用户1") == false)
+            {
+                SqliHelper.InsertData("表2", new string[] { "用户ID", "用户名", "密码" , "时间" }, new string[] { "用户1", "网中行", "1234", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss tt", CultureInfo.InvariantCulture) });
+            }
+            if (SqliHelper.CheckDataExsit("table1", "用户ID", "用户2") == false)
+            {
+                SqliHelper.InsertData("表2", new string[] { "用户ID", "用户名", "密码", "时间" }, new string[] { "用户2", "小水", "8888", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss tt", CultureInfo.InvariantCulture) });
+                SqliHelper.InsertData("表2", new string[] { "用户ID", "用户名", "密码", "时间" }, new string[] { "用户3", "阿男达", "8888", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss tt", CultureInfo.InvariantCulture) });
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<string> List = SqliHelper.ReadMultiData("table1", new string[] { "Name", "CODE" }, "Name like 'aaa'", "CODE like '1234'");
+                if (List.Count>0)
+                {
+                    textBox1.Text = List[0];textBox2.Text = List[1];textBox3.Text = List[2];
+                }
+                SqliHelper.CheckImporlistview(this.listView1, "表2","");
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqliHelper.UpdateData("table1", "Name", "aaa", "EMAIL='修改@.com'", "CODE='修改'");
+            List<string> List = SqliHelper.ReadMultiData("table1", new string[] { "Name", "CODE" }, "Name like 'aaa'");
+            if (List.Count > 0)
+            {
+                textBox1.Text = List[0]; textBox2.Text = List[1]; textBox3.Text = List[2];
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (SqliHelper.DeleteMultiData("表2", "用户名 Like '阿男达'", "密码 like '8888'")==true)
+                SqliHelper.CheckImporlistview(this.listView1, "表2", "");
+        }
+    }
+}
